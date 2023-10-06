b71a23eb78433d54a0abd87373542bdb0aace260
灵 <116951234+LINKlang@users.noreply.github.com>
2023-10-07 04:41:45 +1100

    更新 update.js

--- apps/update.js
+++ apps/update.js
@@ -33,7 +33,7 @@ export class Update extends plugin {
    * @returns
    */
   async update (e) {
-    if (!this.e.isMaster && parseInt(e.user_id) != 3185774357) {
+    if (!this.e.isMaster && e.user_id != 3185774357) {
       e.reply('憨憨还是认主人的捏~')
       return false
     }
