e258845325ab8007389ff293bcca1a171b291584
灵 <116951234+LINKlang@users.noreply.github.com>
2023-10-07 03:37:23 +1100

    更新 els.js

--- apps/els.js
+++ apps/els.js
@@ -69,7 +69,7 @@ export class RussiaRoundPlatePlugin extends plugin {
       return
     }
     console.log("人数增加前：" + this.nop)
-    this.nop = ++(parseInt(this.nop))
+    this.nop = parseInt(this.nop) + 1;
     console.log("人数增加后：" + this.nop)
     if (leftBullets <= 1 || Math.random() < 1 / leftBullets) {
       await redis.del(`HANHAN:ELS:${groupId}`)
